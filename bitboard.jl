d3e059b5777ab49414cc5bd7f92f8c0514811e0f
leesongun <12179851+leesongun@users.noreply.github.com>
2022-04-04 14:52:28 +0900

    copy files from leesongun/othello-nnue

---
+++ bitboard.jl
@@ -0,0 +1,12 @@
+using Bits
+toplane(a::UInt64) = reshape(bits(a), 8, 8, 1, 1)
+
+function bitboard_to_array(a::UInt64)
+    ret = []
+    sizehint!(ret, count_ones(a))
+    while a != 0
+        push!(ret, UInt8(scan1(a) - 1))
+        a &= a - 1
+    end
+    return ret
+end
